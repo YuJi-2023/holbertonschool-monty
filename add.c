a3cf21f40015c670f3a4653d23e5e32126787f5c
cohort20 <sophia.jiyu@gmail.com>
2023-05-10 04:17:58 -0700

    change error message display from stdout to stderr

--- add.c
+++ add.c
@@ -13,7 +13,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if ((*stack) == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stdout, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	total = (*stack)->next->n + (*stack)->n;
